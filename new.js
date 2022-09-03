dfd1520b7597c2740941a4d80d3470a165196743
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-03 20:43:12 +0900

    .test.jsじゃなく.ymlに変更

--- new.js
+++ new.js
@@ -4,6 +4,6 @@ const name = `${time}_${process.argv[2] ?? 'problem'}`;
 
 const cp = [`src/_template.js`, `src/${name}.js`];
 copyFile(...cp);
-copyFile(...cp.map(s => s.replace(/\.js/, '.test.js')));
+copyFile(...cp.map(s => s.replace(/\.js/, '.yml')));
 
 console.log(`created: ${name}.js`);
--- src/abc081a.js
+++ src/abc081a.js
@@ -1,5 +1,5 @@
 input = require('fs').readFileSync(0, 'utf8').trim();
 
-output = input.replace(/[^1]/g, '').length
+output = input.replace(/[^1]/g, '').length;
 
 console.log(output);
