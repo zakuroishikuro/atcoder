9b4ead31ec0ad29b4b8cf169f9c11be8deb9054f
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-03 20:40:19 +0900

    node 12.16.1だからかjestもavaも動かなかったので泥臭くwatch自作する

---
+++ watch.js
@@ -0,0 +1,42 @@
+const { fork } = require('child_process');
+const { watch } = require('chokidar');
+const { load } = require('js-yaml');
+const { readFileSync } = require('fs');
+
+watch('./src', { persistent: true }).on('change', path => {
+  console.log(`*** file changed [${path}] ***`);
+
+  const js = path.replace(/\..+$/, '.js');
+  const yml = path.replace(/\..+$/, '.yml');
+
+  const examples = load(readFileSync(yml));
+
+  let i = 0;
+  let failed = 0;
+  for (let { input, output: expected } of examples) {
+    input = input.trim();
+    expected = expected.trim();
+
+    const { stdin, stdout, stderr } = fork(js, { stdio: 'pipe' });
+    stdout.setEncoding('utf8');
+    stderr.setEncoding('utf8');
+    stdout.on('data', actual => {
+      actual = actual.trim();
+      if (actual != expected) {
+        console.log(`* failed! \n- input: \n${input}\n\n- expected: ${expected}\n- actual: ${actual}\n`);
+        failed++;
+      }
+      if (++i == examples.length) {
+        if (failed == 0) {
+          console.log('ooo all passed! ooo\n');
+        } else {
+          console.log(`xxx ${failed} failed xxx\n`);
+        }
+      }
+    });
+    stderr.on('data', console.error);
+
+    stdin.write(input.trim());
+    stdin.end();
+  }
+});
