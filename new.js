d5012937ae911932f81e7ee0d46d673215d648d9
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-03 21:09:54 +0900

    new.jsがnode v12で動かなかったので作り直した

--- new.js
+++ new.js
@@ -1,9 +1,10 @@
-const { copyFile } = require('fs/promises');
+const { copyFile } = require('fs');
 const time = new Date().toISOString().split(/\D/).slice(0, -3).join('');
-const name = `${time}_${process.argv[2] ?? 'problem'}`;
+const input = (process.argv[2] || 'problem').replace(/\W+/g, '');
+const name = `${time}_${input}`;
 
-const cp = [`src/_template.js`, `src/${name}.js`];
-copyFile(...cp);
-copyFile(...cp.map(s => s.replace(/\.js/, '.yml')));
+const js = [`src/_template.js`, `src/${name}.js`];
+const yml = js.map(s => s.replace(/\.js/, '.yml'));
 
-console.log(`created: ${name}.js`);
+copyFile(...js, () => console.log(`- created: ${js[1]}`));
+copyFile(...yml, () => console.log(`- created: ${yml[1]}`));
