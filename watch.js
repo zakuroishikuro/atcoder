4c3b3da8f98b640a705d6b8b9fd9644892afe384
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-04 11:44:15 +0900

    時間制限を追加 (並列実行なってるけどとりあえずよし)

--- watch.js
+++ watch.js
@@ -6,6 +6,8 @@ const { readFileSync } = require('fs');
 const red = str => '\x1b[31m' + str + '\x1b[0m';
 const green = str => '\x1b[32m' + str + '\x1b[0m';
 
+const TLE_MS = 5000; // Time Limit Exceeded
+
 watch('./src', { persistent: true }).on('change', path => {
   console.log(`*** file changed [${path}] ***`);
 
@@ -15,34 +17,43 @@ watch('./src', { persistent: true }).on('change', path => {
   const examples = load(readFileSync(yml)).slice(1);
 
   let i = 0;
-  let failed = 0;
+  let failedCount = 0;
   for (let { input, output: expected } of examples) {
     input = input.trim();
     expected = [expected].flat().map(e => e.trim());
 
-    const { stdin, stdout, stderr } = fork(js, { stdio: 'pipe' });
+    const child = fork(js, { stdio: 'pipe' });
+    const { stdin, stdout, stderr } = child;
     stdout.setEncoding('utf8');
     stderr.setEncoding('utf8');
     stdout.on('data', actual => {
       actual = actual.trim();
-      if (!expected.includes(actual)) {
+      const failed = !expected.includes(actual);
+      if (failed) {
         process.stdout.write(red('x'));
-        failed++;
+        failedCount++;
       } else {
         process.stdout.write(green('o'));
       }
-      console.log(` actual:`, actual, `expected:`, expected);
+      console.log(` actual:[`, actual, `] expected:`, expected);
+      if (failed) console.log(red(`--- input: \n${input}\n`));
       if (++i == examples.length) {
-        if (failed == 0) {
+        if (failedCount == 0) {
           console.log(green('ooo all passed! ooo\n'));
         } else {
-          console.log(red(`xxx ${failed} failed xxx\n`));
+          console.log(red(`xxx ${failedCount} failed xxx\n`));
         }
       }
+      clearTimeout(tleTimer);
     });
     stderr.on('data', console.error);
 
     stdin.write(input);
     stdin.end();
+
+    const tleTimer = setTimeout(() => {
+      console.log(red(`x Time Limit Exceeded:\n--- input: \n${input}\n`));
+      child.kill();
+    }, TLE_MS);
   }
 });
