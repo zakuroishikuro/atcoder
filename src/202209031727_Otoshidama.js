0b38b0d0048eed787ffd2873933d657e7c063185
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-04 15:36:01 +0900

    Otoshidama 解説読んでやり直し

--- src/202209031727_Otoshidama.js
+++ src/202209031727_Otoshidama.js
@@ -1,21 +1,17 @@
 console.log(main(require('fs').readFileSync(0, 'utf8').trim()));
 
 function main(input) {
-  const [bills, amount] = input.split(/\s/).map(s => +s);
+  const [bills, target] = input.split(/\s/).map(s => +s);
 
-  // i: 1000 yen
+  // i: 10000 yen
   for (let i = 0; i <= bills; i++) {
     // j: 5000 yen
-    for (let j = 0; j <= bills; j++) {
-      const cnt = i + j;
-      const amt = i * 1000 + j * 5000;
-      if (cnt > bills || amt > amount) break;
-      // k: 10000 yen
-      for (let k = 0; k <= bills; k++) {
-        const cnt = i + j + k;
-        const amt = i * 1000 + j * 5000 + k * 10000;
-        if (cnt > bills || amt > amount) break;
-        if (cnt === bills && amt === amount) return [k, j, i].join(' ');
+    for (let j = 0; j <= bills - i; j++) {
+      // k: 1000 yen
+      const k = bills - i - j;
+      const amt = i * 10000 + j * 5000 + k * 1000;
+      if (amt == target) {
+        return [i, j, k].join(' ');
       }
     }
   }
