577fb0f676aab500ddd8e37d5164a1e3a2f0000a
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-04 23:02:24 +0900

    クソfizzbuzz2

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,5 +2,5 @@ console.log(main(require('fs').readFileSync(0, 'utf8').trim()));
 
 function main(input) {
   const [first, last] = input.split(/\s+/);
-  return [...Array(last - first + 1)].map((_,i)=>[["Fizz"][++i % 3], ["Buzz"][i % 5]].join("") || i).join(" ")
+  return [...Array(last - first + 1)].map((_, i) => [++i, 'Fizz', 'Buzz', 'FizzBuzz'][!(i % 3) + !(i % 5) * 2]).join(' ');
 }
