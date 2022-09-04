385f5bda8063a28eb11317390195fdb52699baf9
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-04 15:44:34 +0900

    リベンジ

--- src/202209040342_白昼夢.js
+++ src/202209040342_白昼夢.js
@@ -1,6 +1,7 @@
 console.log(main(require('fs').readFileSync(0, 'utf8').trim()));
 
 function main(input) {
-  // 無理。わかんない。
-  return input.trim().replace(/dreamereraser|dreameraser|dreamer|dream|eraser|erase/g, "") ? "NO" : "YES";
+  const r = /(dreamer|dream|eraser|erase)$/;
+  while (r.test(input)) input = input.replace(r, "");
+  return input ? "NO" : "YES";
 }
