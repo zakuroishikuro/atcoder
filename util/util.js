7e42178a9c21e9dcbc9b3a5d6974417de829cb41
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-06 07:44:20 +0900

    utilを修正
    * sum関数を追加
    * range関数でstepを指定できるよう修正 (Python風)
    * module.exporstを最後にするよう変更

--- util/util.js
+++ util/util.js
@@ -1,20 +1,38 @@
-module.exports.lines = str => str.split(/\r?\n|\r/);
-module.exports.words = str => str.trim().split(/\s+/);
-module.exports.ints = str => Int32Array.from(str.trim().split(/\s+/));
-module.exports.floats = str => Float64Array.from(str.trim().split(/\s+/));
+// そのまま使うかは別としてメモ代わりのものも書いておく
+const lines = str => str.split(/\r?\n|\r/);
+const words = str => str.trim().split(/\s+/);
+const ints = str => Int32Array.from(str.trim().split(/\s+/));
+const floats = str => Float64Array.from(str.trim().split(/\s+/));
 
-module.exports.sortBy = (arr, map = v => v, des = 0) =>
+const sortBy = (arr, map = v => v, des = 0) =>
   arr
     .map(v => [map(v), v])
     .sort([([a], [b]) => a - b, ([a], [b]) => b - a][des])
     .map(v => v[1]);
 
-module.exports.range = (first, last) => {
-  if (!last) [first, last] = [0, first];
-  return [...Array(last - first)].map((_, i) => i + first);
+const range = (first, last = first, step = 1) => {
+  first = (first != last) * first;
+  let n = first - step;
+  return Int32Array.from(Array(Math.floor((last - first) / step)), () => (n += step));
 };
 
-module.exports.sum = nums => nums.reduce((a, b) => a + b);
-// 等差数列の和の公式
-module.exports.sum1toN = n => (n * (n + 1)) / 2;
-module.exports.sumNtoM = (n, m) => sum1toN(m) - sum1toN(n);
+const sum = nums => nums.reduce((a, b) => a + b);
+// 三角数, 等差数列の和の公式
+const sum1toN = n => (n * (n + 1)) / 2;
+const sumNtoM = (n, m) => sum1toN(m) - sum1toN(n - 1);
+
+// 四角数, 平方数
+const sum1toN_step2 = n => Math.floor(n / 2) ** 2;
+
+module.exports = {
+  lines,
+  words,
+  ints,
+  floats,
+  sortBy,
+  range,
+  sum,
+  sum1toN,
+  sumNtoM,
+  sum1toN_step2,
+};
--- util/util.test.js
+++ util/util.test.js
@@ -1,31 +1,50 @@
-const { range, sortBy, sum, sum1toN, sumNtoM } = require('./util');
+const { range, sortBy, sum, sum1toN, sumNtoM, sum1toN_step2 } = require('./util');
 
 test('range', () => {
   // length
-  expect(range(10)).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(range(10)).toEqual(Int32Array.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-  // first..last
-  expect(range(5, 10)).toStrictEqual([5, 6, 7, 8, 9]);
+  // first...last
+  expect(range(5, 10)).toEqual(Int32Array.of(5, 6, 7, 8, 9));
+
+  // first...last, step
+  expect(range(10, 20, 2)).toEqual(Int32Array.of(10, 12, 14, 16, 18));
 });
 
 test('sortBy', () => {
   // Ascending
-  expect(sortBy(['aa', 'b', 'ccc'], v => v.length)).toStrictEqual(["b", "aa", "ccc"]);
+  expect(sortBy(['aa', 'b', 'ccc'], v => v.length)).toEqual(['b', 'aa', 'ccc']);
 
   // Descending
-  expect(sortBy(['aa', 'b', 'ccc'], v => v.length, 1)).toStrictEqual(["ccc", "aa", "b"]);
+  expect(sortBy(['aa', 'b', 'ccc'], v => v.length, 1)).toEqual(['ccc', 'aa', 'b']);
 });
 
 test('sum1toN', () => {
-  // 1 ～ 100
-  const nums = range(1, 101);
-  expect(sum(nums)).toBe(5050);
-  expect(sum1toN(100)).toBe(5050);
-})
+  // 1..10
+  expect(sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(55);
+  expect(sum1toN(10)).toBe(55);
 
-test('sumNtoM', ()=>{
-  // 100 ～ 555
-  const nums = range(1, 556);
-  expect(sum(nums)).toBe(154290);
-  expect(sum1toN(555)).toBe(154290);
+  // 1..100
+  expect(sum(range(1, 101))).toBe(5050);
+  expect(sum1toN(100)).toBe(5050);
+});
+
+test('sumNtoM', () => {
+  // 5..10
+  expect(sum([5, 6, 7, 8, 9, 10])).toBe(45);
+  expect(sumNtoM(5, 10)).toBe(45);
+
+  // 100..555
+  expect(sum(range(100, 556))).toBe(149340);
+  expect(sumNtoM(100, 555)).toBe(149340);
+});
+
+test('sum1toN_step2', () => {
+  // 1..10 step 2
+  expect(sum([1, 3, 5, 7, 9])).toBe(25);
+  expect(sum1toN_step2(10)).toBe(25);
+
+  // 1..555 step 2
+  expect(sum(range(1, 556, 2))).toBe(76729);
+  expect(sum1toN_step2(555)).toBe(76729);
 });
