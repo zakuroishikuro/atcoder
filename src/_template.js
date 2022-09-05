9a98ebaf64d2d4246dbb8117176af21108b87b0c
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-06 01:26:07 +0900

    テンプレを整備

--- src/_template.js
+++ src/_template.js
@@ -1,19 +1,12 @@
-function main(input) {
-  return input.ints[0];
-}
-
+const lines = str => str.split(/\r?\n|\r/);
+const words = str => str.trim().split(/\s+/);
+const ints = str => Int32Array.from(str.trim().split(/\s+/));
+const floats = str => Float64Array.from(str.trim().split(/\s+/));
 // prettier-ignore
-{
-  Object.defineProperties(String.prototype, {
-    lines: {get(){return this.split(/\r?\n|\r/)}},
-    words: {get(){return this.trim().split(/\s+/)}},
-    chars: {get(){return [...this]}},
-    ints:  {get(){return Int32Array.from(this.words)}},
-    floats:{get(){return Float64Array.from(this.words)}},
-  });
-  Object.defineProperties(Array.prototype, {
-    sortBy: {value(map=v=>v, des=0){f=[(a,b)=>a-b,(a,b)=>b-a][des]||des;return this.map(v=>[map(v),v]).sort(([a],[b])=>f(a,b)).map(v=>v[1])}},
-  })
+const sortBy = (arr,map=v=>v,des=0)=>{var f=[(a,b)=>a-b,(a,b)=>b-a][des]||des;return arr.map(v =>[map(v),v]).sort(([a],[b])=>f(a,b)).map(v=>v[1])};
+
+function main(input) {
+  return words(input)[0];
 }
 
 console.log(main(require('fs').readFileSync(0, 'utf8')));
