79efc205796830e29337552c650cf17224657ce2
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-05 23:28:43 +0900

    ユーティリティメソッドを整備

--- src/_template.js
+++ src/_template.js
@@ -8,8 +8,8 @@ function main(input) {
     lines: {get(){return this.split(/\r?\n|\r/)}},
     words: {get(){return this.trim().split(/\s+/)}},
     chars: {get(){return [...this]}},
-    ints:  {get(){return Int32Array.of(this.words)}},
-    floats:{get(){return Float64Array.of(this.words)}},
+    ints:  {get(){return Int32Array.from(this.words)}},
+    floats:{get(){return Float64Array.from(this.words)}},
   });
   Object.defineProperties(Array.prototype, {
     sortBy: {value(map=v=>v, des=0){f=[(a,b)=>a-b,(a,b)=>b-a][des]||des;return this.map(v=>[map(v),v]).sort(([a],[b])=>f(a,b)).map(v=>v[1])}},
