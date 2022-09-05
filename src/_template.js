87c3776a1608efdd9f655447eaed121e8f83a8e4
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-05 23:23:24 +0900

    ユーティリティメソッドを整備

--- src/_template.js
+++ src/_template.js
@@ -1,22 +1,15 @@
 function main(input) {
-  return input.nums[0];
+  return input.ints[0];
 }
 
 // prettier-ignore
 {
   Object.defineProperties(String.prototype, {
-    strip: {get(){return this.trim()}},
     lines: {get(){return this.split(/\r?\n|\r/)}},
-    words: {get(){return this.strip.split(/\s+/)}},
+    words: {get(){return this.trim().split(/\s+/)}},
     chars: {get(){return [...this]}},
-    nums:  {get(){return Int32Array.of(this.words)},
-    to_i: {get(){return parseInt(this)}},
-    to_f: {get(){return parseFloat(this)}},
-  }});
-  Object.defineProperties(Number.prototype, {
-    ceil: {get(){return Math.ceil(this)}},
-    floor: {get(){return Math.floor(this)}},
-    to_s: {get(){return this.toString(10)}},
+    ints:  {get(){return Int32Array.of(this.words)}},
+    floats:{get(){return Float64Array.of(this.words)}},
   });
   Object.defineProperties(Array.prototype, {
     sortBy: {value(map=v=>v, des=0){f=[(a,b)=>a-b,(a,b)=>b-a][des]||des;return this.map(v=>[map(v),v]).sort(([a],[b])=>f(a,b)).map(v=>v[1])}},
