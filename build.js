b59ab6ccb6fb3a171060f830fbcaa16b8d3f04fe
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-06 02:14:47 +0900

    utilをminifyする機能を追加

---
+++ build.js
@@ -0,0 +1,21 @@
+const { readFile, writeFile } = require('fs/promises');
+const { build } = require('esbuild');
+
+const src = 'util/util.js';
+const min = 'util/util.min.js';
+
+(async () => {
+  await build({
+    entryPoints: [src],
+    outfile: min,
+    bundle: true,
+    minify: true,
+  });
+
+  // 雑すぎん？
+  console.log("[minified util]")
+  let js = await readFile(min, 'utf-8');
+  js = js.replace(/}\);i\(\);}\)\(\);$/m, ";").replace(/.\.exports\./g, '\nconst ').split(/\n/).slice(1).map(s=>s.trim()).join("\n");
+  console.log(js);
+  await writeFile(min, js, 'utf-8');
+})();
---
+++ util/util.js
@@ -0,0 +1,17 @@
+module.exports.lines = str => str.split(/\r?\n|\r/);
+module.exports.words = str => str.trim().split(/\s+/);
+module.exports.ints = str => Int32Array.from(str.trim().split(/\s+/));
+module.exports.floats = str => Float64Array.from(str.trim().split(/\s+/));
+
+module.exports.sortBy = (arr, map = v => v, des = 0) => {
+  var f = [(a, b) => a - b, (a, b) => b - a][des] || des;
+  return arr
+    .map(v => [map(v), v])
+    .sort(([a], [b]) => f(a, b))
+    .map(v => v[1]);
+};
+
+module.exports.range = (first, last) => {
+  if (!last) [first, last] = [0, first];
+  return [...(first - last)].map((_, i) => i + first);
+};
