057571233c557c7887b5662821ab5115f41a1419
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-05 23:16:43 +0900

    テンプレにユーティリティメソッド群を追加

--- src/_template.js
+++ src/_template.js
@@ -1,6 +1,26 @@
-console.log(main(require('fs').readFileSync(0, 'utf8').trim()));
-
 function main(input) {
-  const [a, b] = input.split(/\s+/).map(s => +s);
-  return a
+  return input.nums[0];
 }
+
+// prettier-ignore
+{
+  Object.defineProperties(String.prototype, {
+    strip: {get(){return this.trim()}},
+    lines: {get(){return this.split(/\r?\n|\r/)}},
+    words: {get(){return this.strip.split(/\s+/)}},
+    chars: {get(){return [...this]}},
+    nums:  {get(){return Int32Array.of(this.words)},
+    to_i: {get(){return parseInt(this)}},
+    to_f: {get(){return parseFloat(this)}},
+  }});
+  Object.defineProperties(Number.prototype, {
+    ceil: {get(){return Math.ceil(this)}},
+    floor: {get(){return Math.floor(this)}},
+    to_s: {get(){return this.toString(10)}},
+  });
+  Object.defineProperties(Array.prototype, {
+    sortBy: {value(map=v=>v, des=0){f=[(a,b)=>a-b,(a,b)=>b-a][des]||des;return this.map(v=>[map(v),v]).sort(([a],[b])=>f(a,b)).map(v=>v[1])}},
+  })
+}
+
+console.log(main(require('fs').readFileSync(0, 'utf8')));
