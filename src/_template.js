1c9b22e941fcd723ee55f10b6e694751f55020ac
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2022-09-10 23:26:34 +0900

    テンプレ修正

--- src/_template.js
+++ src/_template.js
@@ -1,6 +1,6 @@
 function main(input) {
-  const [a] = input.trim().split(/\s+/);
+  const [a] = input.split(/\s+/);
   return a;
 }
 
-console.log(main(require('fs').readFileSync(0, 'utf8')));
+console.log(main(require('fs').readFileSync(0, 'utf8').trim()));
